ebf9866e91c3b342fc7706e5e3efa28182bf4d30
HIBER97 <hibar9710@gmail.com>
2022-02-19 10:44:47 +0900

    2022. 2. 19. AM 10:44 --------------------------------------- 0x0B "재귀" 완료 "Q.1074" 완료

--- Q.1074.cpp
+++ Q.1074.cpp
@@ -2,9 +2,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int funZ (int n, int m, int o){
+    if(n == 0)
+        return 0;
+    int half = 1 << (n - 1);
+    if(m < half && o < half)
+        return funZ(n - 1, m, o);
+    if(m < half && o >= half)
+        return half * half + funZ(n - 1, m, o - half);
+    if(m >= half && o < half)
+        return 2*half*half + funZ(n-1, m - half, o);
+    return 3*half*half + funZ(n-1, m - half, o - half);
+}
+
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
+
+    int n,m,o;
+    cin >> n >> m >> o;
+    cout << funZ(n,m,o);
+
 
 }
