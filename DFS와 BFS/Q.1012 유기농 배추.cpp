99dca28583ae2635647655b17793be95debf5d82
HIBER97 <hibar9710@gmail.com>
2022-02-21 15:24:22 +0900

    2022. 2. 20. PM 4:26 --------------------------------------- 0x0A "DFS" 복습 "Q.2667 단지번호 붙이기" 완료 "Q.1012 유기동 배추" 시도

---
+++ DFS와 BFS/Q.1012 유기농 배추.cpp
@@ -0,0 +1,53 @@
+// https://www.acmicpc.net/problem/1012
+#include <bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+int board[52][52];
+bool vis[52][52];
+int R;
+int m, n, k;
+int Locx, Locy;
+int area = 0;
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> R;
+    for (int i = 0; i < R; i++) {
+        cin >> m >> n >> k;
+
+        for (int j = 0; j <= k; j++) {
+            cin >> Locx >> Locy;
+            board[Locy][Locy] = 1;
+        }
+
+        for (int j = 0; j < m; j++) {
+            for (int l = 0; l < n; l++) {
+                if (board[j][l] == 0 || vis[j][l])
+                    continue;
+                    queue<pair<int,int>> Q;
+                    vis[j][l] = 1;
+                    Q.pop();
+
+                    while (!Q.empty()){
+                        area++;
+                        auto cur = Q.front(); Q.pop();
+                        for (int dir = 0; dir < 4; dir++) {
+                            int nx = cur.X + dx[dir];
+                            int ny = cur.Y + dy[dir];
+                            if (nx < 0 || nx >= m || ny < 0 || ny >= n)
+                                continue;
+                            if (vis[nx][ny] || board[nx][ny] != 1)
+                                continue;
+                            vis[nx][ny] = 1;
+                            Q.push({nx,ny});
+                        }
+                        cout << area;
+                    }
+            }
+        }
+    }
+}
--- DFS와 BFS/Q.2667 단지번호붙이기.cpp
+++ DFS와 BFS/Q.2667 단지번호붙이기.cpp
@@ -1,4 +1,4 @@
-// https://www.acmicpc.net/problem/1926
+// https://www.acmicpc.net/problem/2667
 #include <bits/stdc++.h>
 using namespace std;
 #define X first
@@ -6,7 +6,6 @@ using namespace std;
 int board[30][30];
 bool vis[30][30];
 int x, y;
-int area = 0;
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
 
@@ -15,20 +14,29 @@ int main() {
     cin.tie(0);
     cin >> x;
     y = x;
-    for (int i = 0; i < x; i++)
-        for (int j = 0; j < y; j++)
-            cin >> board[i][j];
+    vector<int> Result;
+    int Num = 0;
+
+    for (int i = 0; i < x; i++){
+        string st;
+        cin >> st;
+        for (int j = 0; j < x; j++) {
+            board[i][j] = st[j] - '0';
+        }
+    }
+
 
     for (int i = 0; i < x; i++){
         for (int j = 0; j < y; j++){
             if (board[i][j] == 0 || vis[i][j])
                 continue;
 
+            Num++;
             queue<pair<int,int>> Q;
             vis[i][j] = 1;
             Q.push({i,j});
 
-
+            int area = 0;
 
             while (!Q.empty()){
                 area++;
@@ -42,10 +50,14 @@ int main() {
                         continue;
                     vis[nx][ny] = 1;
                     Q.push({nx,ny});
+                    }
                 }
+            Result.push_back(area);
             }
-
-        }
     }
-    cout << area;
+    cout << Result.size() << "\n";
+    sort(Result.begin(),Result.end());
+    for (int j = 0; j < Result.size(); j++)
+        cout << Result[j] << '\n';
 }
+
