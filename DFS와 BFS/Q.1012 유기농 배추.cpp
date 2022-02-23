acae0c14c3e8c8996f398594bb4e5504932b34bf
HIBER97 <hibar9710@gmail.com>
2022-02-23 18:57:28 +0900

    2022. 2. 23. PM 6:57 --------------------------------------- 0x0A "DFS" 복습 "Q.1012 유기동 배추" 완료 "Q.7569 토마토" 시도

--- DFS와 BFS/Q.1012 유기농 배추.cpp
+++ DFS와 BFS/Q.1012 유기농 배추.cpp
@@ -1,52 +1,61 @@
 // https://www.acmicpc.net/problem/1012
 #include <bits/stdc++.h>
 using namespace std;
-#define X first
-#define Y second
-int board[52][52];
-bool vis[52][52];
+#define Y first
+#define X second
+int board[51][51];
+bool vis[51][51];
 int R;
 int m, n, k;
 int Locx, Locy;
-int area = 0;
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
+
+void reset(){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            board[i][j] = 0;
+            vis[i][j] = 0;
+        }
+    }
+}
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> R;
-    for (int i = 0; i < R; i++) {
+    while (R--) {
         cin >> m >> n >> k;
-
-        for (int j = 0; j < k; j++) {
+        reset();
+        while (k--) {
             cin >> Locx >> Locy;
             board[Locy][Locx] = 1;
         }
-
+        int area = 0;
         for (int j = 0; j < n; j++) {
             for (int l = 0; l < m; l++) {
                 if (board[j][l] == 0 || vis[j][l])
                     continue;
-                    queue<pair<int,int>> Q;
-                    vis[j][l] = 1;
-                    Q.pop();
-                    while (!Q.empty()){
-                        auto cur = Q.front(); Q.pop();
-                        for (int dir = 0; dir < 4; dir++) {
-                            int nx = cur.X + dx[dir];
-                            int ny = cur.Y + dy[dir];
-                            if (nx < 0 || nx >= m || ny < 0 || ny >= n)
-                                continue;
-                            if (vis[nx][ny] || board[nx][ny] != 1)
-                                continue;
-                            vis[nx][ny] = 1;
-                            Q.push({nx,ny});
-                        }
-                        area++;
+                area++;
+                board[j][l] = 1;
+
+                queue<pair<int,int>> Q;
+                Q.push({j,l});
+                while (!Q.empty()){
+                    auto cur = Q.front(); Q.pop();
+                    for (int dir = 0; dir < 4; dir++) {
+                        int nx = cur.X + dx[dir];
+                        int ny = cur.Y + dy[dir];
+                        if (nx < 0 || nx >= m || ny < 0 || ny >= n)
+                            continue;
+                        if (board[ny][nx] != 1 || vis[ny][nx])
+                            continue;
+                        Q.push({ny,nx});
+                        vis[ny][nx] = 1;
                     }
+                }
             }
         }
-        cout << area;
+        cout << area << '\n';
     }
 }
---
+++ DFS와 BFS/Q.7569 토마토.cpp
@@ -0,0 +1,17 @@
+// https://www.acmicpc.net/problem/7569
+#include <bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+int board[51][51];
+bool vis[51][51];
+
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> R;
+
+}
