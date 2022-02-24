ad816f03cd65b4dd8091c94d02847a75b1f80bd4
HIBER97 <hibar9710@gmail.com>
2022-02-24 20:21:14 +0900

    2022. 2. 24. PM 8:20 --------------------------------------- 0x0A "DFS" 복습 "Q.1012 유기동 배추" 완료 "Q.7569 토마토" 시도

--- DFS와 BFS/Q.7569 토마토.cpp
+++ DFS와 BFS/Q.7569 토마토.cpp
@@ -3,15 +3,70 @@
 using namespace std;
 #define X first
 #define Y second
-int board[51][51];
-bool vis[51][51];
+#define Z Third
+int board[102][102][102];
+bool vis[102][102][102];
+int M,N,H;
+int dx[] = {0,0,1,-1,0,0};
+int dy[] = {1,-1,0,0,0,0};
+int dz[] = {0,0,0,0,1,-1};
 
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
+void reset(){
+    for (int i = 0; i < H; i++)
+        for (int j = 0; j < N; j++)
+            for (int k = 0; k < M; k++){
+                board[i][j][k] = -1;
+                vis[i][j][k] = 0;
+            }
+
+}
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cin >> R;
+    cin >> M >> N >> H;
+
+    reset();
+
+    /*vector<string> board(n);
+    for(string& i : board) cin >> i;
+     벡터로 입력 받기*/
+
+    for (int i = 0; i < H; i++)
+        for (int j = 0; j < N; j++)
+            for (int k = 0; k < M; k++)
+                cin >> board[i][j][k];
+
+    for (int i = 0; i < H; i++){
+        for (int j = 0; j < N; j++){
+            for (int k = 0; k < M; k++){
+                if (board[i][j][k] == 0 || vis[i][j][k])
+                    continue;
+                board[i][j][k] = 1;
+
+                queue<tuple<int,int,int>> Q;
+                Q.push({i,j,k});
+
+                while (!Q.empty()){
+                    auto [nx,ny,nz] = Q.front(); Q.pop();
+
+                    for (int dir = 0; dir < 6; dir++) {
+                        int nx = [nx,ny,nz].X + dx[dir];
+                        int ny = [nx,ny,nz].Y + dy[dir];
+                        int nz = [nx,ny,nz].Z + dz[dir];
+
+                    }
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
 
 }
