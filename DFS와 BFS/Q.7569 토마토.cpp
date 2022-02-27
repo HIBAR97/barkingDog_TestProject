d454457734e090680d214bfcfd89bb441e26df4b
HIBER97 <hibar9710@gmail.com>
2022-02-27 13:24:11 +0900

    2022. 2. 27. PM 1:23 ------------------------------------- 0x0A "DFS" 복습 "Q.7569 토마토" 틀림 "0,-1"출력은 완성 Days 출력 오류

--- DFS와 BFS/Q.7569 토마토.cpp
+++ DFS와 BFS/Q.7569 토마토.cpp
@@ -9,6 +9,7 @@ int M,N,H;
 int Tomato = 0;
 int Days = 0;
 queue<tuple<int,int,int>> Q;
+
 int dx[] = {0,0,1,-1,0,0};
 int dy[] = {1,-1,0,0,0,0};
 int dz[] = {0,0,0,0,1,-1};
@@ -34,9 +35,9 @@ int main() {
     for(string& i : board) cin >> i;
      벡터로 입력 받기*/
 
-    for (int i = 0; i < M; i++){
+    for (int i = 0; i < H; i++){
         for (int j = 0; j < N; j++){
-            for (int k = 0; k < H; k++){
+            for (int k = 0; k < M; k++){
                 cin >> board[i][j][k];
                 if (board[i][j][k] == 0)
                     Tomato++;
@@ -47,9 +48,6 @@ int main() {
             }
         }
     }
-
-
-
 
     if (Tomato == 0){
         cout << Tomato;
@@ -71,7 +69,7 @@ int main() {
 
                 if (nx < 0 || nx >= M || ny < 0 || ny >= N || nz < 0 || nz >= H )
                     continue;
-                if (board[nx][ny][nz] != 1 || vis[nx][ny][nz])
+                if (board[nx][ny][nz] == 1 || vis[nx][ny][nz])
                     continue;
                 Q.push({nx,ny,nz});
                 vis[nx][ny][nz] = vis[X][Y][Z] + 1;
@@ -80,12 +78,12 @@ int main() {
         }
         Days++;
     }
+
     if (Tomato > 0){
         cout << -1;
     } else{
         cout << Days - 1 << '\n';
     }
-
 }
 
 
