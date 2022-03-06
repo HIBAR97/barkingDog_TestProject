b2e95f62a467f9a11cddf2e5bdeacb1210fda894
HIBER97 <hibar9710@gmail.com>
2022-03-06 14:36:27 +0900

    2022. 3. 6. PM 2:35 --------------------------------------- 0x0A "DFS" 복습 "Q.2206 벽 부수고 이동하기" 시도 해결 단계 기획중

--- DFS와 BFS/Q.2206 벽 부수고 이동하기.cpp
+++ DFS와 BFS/Q.2206 벽 부수고 이동하기.cpp
@@ -3,8 +3,10 @@
 using namespace std;
 #define X first
 #define Y second
-int board[1002][1002];
-bool vis[1002][1002];
+int board[1002];
+int vis[1002][1002];
+int dis[1002][1002];
+string board;
 int N, M;
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
@@ -14,15 +16,14 @@ int main() {
     cin.tie(0);
     cin >> N >> M;
 
-    for (int i = 0; i < M; i++)
-        for (int j = 0; j < N; j++)
-            cin >> board[i][j];
+    cin >> board[];
+
 
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < N; j++) {
             if (board[i][j] == 0 ||vis[i][j])
                 continue;
-            board[i][j] = 1;
+            vis[i][j] = 1;
 
             queue<pair<int, int>> Q;
             Q.push({i,j});
@@ -35,10 +36,14 @@ int main() {
                         continue;
                     if (board[nx][ny] != 1 || vis[nx][ny])
                         continue;
+                    vis[i][j] = dis[nx][ny] + 1;
                     Q.push({nx, ny});
                     vis[nx][ny] = 1;
                 }
             }
+            Q.push({N - 1, M - 1});
+            dis[N-1][M-1] = 1;
+
         }
         cout << "1";
     }
