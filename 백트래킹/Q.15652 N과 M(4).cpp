0e02c7ec565f1f07b45a60c6be5830e6e6d244a0
HIBER97 <hibar9710@gmail.com>
2022-07-04 10:12:50 +0900

    2022. 7. 4. AM 10:12 --------------------------------------- "Q.15649 N과M(4)" 완성 "Q.9663 N-Queen" 시도

--- 백트래킹/Q.15652 N과 M(4).cpp
+++ 백트래킹/Q.15652 N과 M(4).cpp
@@ -1,1 +1,34 @@
 //https://www.acmicpc.net/problem/15652
+#include <bits/stdc++.h>
+using namespace std;
+int N,M;
+int arr[10];
+bool used[10];
+
+void func(int k){
+    if( k == M ){
+        for(int i = 0; i < M; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    int start = 1;
+    if(k != 0)
+        start = arr[k-1];
+
+    for (int i = start; i <= N; i++) {
+        if(!used[i]){
+            arr[k] = i;
+            //used[i] = 1;
+            func(k+1);
+            //used[i] = 0;
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    func(0);
+}
