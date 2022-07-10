a4011ea67f56fca213ca634d399cf34ca686a67c
HIBER97 <hibar9710@gmail.com>
2022-07-10 15:17:14 +0900

    2022. 7. 10. PM 3:16 --------------------------------------- "Q.14888 연산자 끼워넣기" 완성

--- 백트래킹/Q.14888 연산자 끼워넣기.cpp
+++ 백트래킹/Q.14888 연산자 끼워넣기.cpp
@@ -10,23 +10,23 @@ int Result_Min = 1000000001;
 
 void func(int R, int idx){
     if (idx == N){
-        if (R < Result_Max)
+        if (R > Result_Max)
             Result_Max = R;
-        if (R > Result_Min)
+        if (R < Result_Min)
             Result_Min = R;
         return;
     }
     for (int i = 0; i < 4; i++) {
         if(arr2[i] > 0){
-            arr[i]--;
+            arr2[i]--;
             if(i == 0)
-                func(R + arr2[idx], idx + 1);
+                func(R + arr[idx], idx + 1);
             else if (i == 1)
-                func(R - arr2[idx], idx + 1);
+                func(R - arr[idx], idx + 1);
             else if (i == 2)
-                func(R * arr2[idx], idx + 1);
+                func(R * arr[idx], idx + 1);
             else if (i == 3)
-                func(R / arr2[idx], idx + 1);
+                func(R / arr[idx], idx + 1);
             arr2[i]++;
         }
     }
@@ -40,9 +40,8 @@ int main(){
     for (int i = 0; i < N; i++)
         cin >> arr[i];
 
-    for (int i = 0; i < 4; i++){
+    for (int i = 0; i < 4; i++)
         cin >> arr2[i];
-    }
 
     func(arr[0],1);
     cout << Result_Max << '\n';
