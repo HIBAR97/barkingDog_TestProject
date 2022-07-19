569f0f2e7361c867e7b22e812e005ebcbe363c07
HIBER97 <hibar9710@gmail.com>
2022-07-19 16:00:55 +0900

    2022. 7. 19. PM 4:00 --------------------------------------- "Q.18808 스티커 붙이기" 시도
    입력, 회전 함수 작성
    알고리즘 구상중

--- 시뮬레이션/Q.18808 스티커 붙이기.cpp
+++ 시뮬레이션/Q.18808 스티커 붙이기.cpp
@@ -1,1 +1,37 @@
 //https://www.acmicpc.net/problem/18808
+#include <bits/stdc++.h>
+using namespace std;
+
+int N,M,K;
+int R,C;
+int Block[20][20][20];
+
+void Rotate(int x, int y, int idx){
+    for (int i = 0; i < idx; i++)
+        for (int j = 0; j < x ; j++)
+            for (int k = 0; k < y; k++)
+                Block[idx][x][y] = Block[idx][3-1-y][x];
+
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> M >> K;
+
+    //입력
+    for (int i = 0; i < K; i++) {
+        cin >> R >> C;
+        for (int j = 0; j < R; j++)
+            for (int k = 0; k < C; k++)
+                cin >> Block[K][N][M];
+
+    }
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < M; j++)
+
+
+
+}
