309273bdbcf8de71b4eeadac56a462fb08328948
HIBER97 <hibar9710@gmail.com>
2022-07-21 09:10:33 +0900

    2022. 7. 21. AM 9:10 --------------------------------------- "Q.18808 스티커 붙이기" 시도
    알고리즘 구상중

--- 시뮬레이션/Q.18808 스티커 붙이기.cpp
+++ 시뮬레이션/Q.18808 스티커 붙이기.cpp
@@ -17,10 +17,12 @@ void Move(int x, int y, int idx){
 }
 
 void Rotate(int x, int y, int idx){
+    int Change[20][20][20];
+
     for (int i = 0; i < idx; i++)
         for (int j = 0; j < x ; j++)
             for (int k = 0; k < y; k++)
-                Block[idx][x][y] = Block[idx][3-1-y][x];
+                Change[idx][x][y] = Block[idx][3-1-y][x];
 
 }
 
@@ -35,12 +37,19 @@ int main(){
         cin >> R >> C;
         for (int i = 0; i < R; i++)
             for (int j = 0; j < C; j++)
-                cin >> Block[i][j];
+                cin >> Block[K][i][j];
 
         //풀이 시작
         for (int rotate = 0; rotate < 4; rotate++){
             bool is_paste = false;
-
+            for (int x = 0; x < N-r; x++)
+                if(is_paste)
+                    break;
+            for (int y = 0; y <= M-c ; y++)
+                if(Move(x,y)){
+                    is_paste = true;
+                    break;
+                }
         }
     }
 }
