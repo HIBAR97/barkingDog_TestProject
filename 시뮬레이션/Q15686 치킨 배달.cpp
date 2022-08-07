e2b11c0cecd6a52aa0963e4915c07d6829f8a3ff
HIBER97 <hibar9710@gmail.com>
2022-08-07 21:34:34 +0900

    2022. 8. 7. PM 9:33 --------------------------------------- "Q.15686 치킨 배달" 시도중

---
+++ 시뮬레이션/Q15686 치킨 배달.cpp
@@ -0,0 +1,20 @@
+//https://www.acmicpc.net/problem/15686
+#include <bits/stdc++.h>
+using namespace std;
+
+int N,M;
+int Map[]
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            cin >> Map[i][j];
+
+    
+}
+
