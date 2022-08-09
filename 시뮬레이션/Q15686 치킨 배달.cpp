f888a0dd02eeb1fa7ded26320bcf541642918c11
HIBER97 <hibar9710@gmail.com>
2022-08-09 10:08:03 +0900

    2022. 8. 9. AM 10:07 --------------------------------------- "Q.15686 치킨 배달" 시도중 뭐 빠진거 같은데 거리 계산하는 쪽

--- 시뮬레이션/Q15686 치킨 배달.cpp
+++ 시뮬레이션/Q15686 치킨 배달.cpp
@@ -2,8 +2,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define X first
+#define Y second
+
 int N,M;
-int Map[]
+int Map[50][50];
+
+vector<pair<int,int>> chichen;
+vector<pair<int,int>> store;
 
 int main() {
     ios::sync_with_stdio(0);
@@ -11,10 +17,33 @@ int main() {
 
     cin >> N >> M;
 
-    for (int i = 0; i < N; ++i)
-        for (int j = 0; j < N; ++j)
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++){
             cin >> Map[i][j];
+            if (Map[i][j] == 1)
+                store.push_back({i,j});
+            if (Map[i][j] == 2)
+                chichen.push_back({i,j});
+        }
 
-    
+    vector<int> brute(chichen.size(),1);
+    fill(brute.begin(), brute.begin() + chichen.size() - M, 0);
+
+    int mn = 0;
+
+    do {
+        int dis = 0;
+        for (auto Store : store) {
+            for (int i = 0; i < chichen.size(); i++) {
+                if (brute[i] == 0)
+                    continue;
+
+
+            }
+        }
+    mn = min(mn,dis);
+    }while (next_permutation(brute.begin(), brute.end()));
+
+    cout << mn;
 }
 
