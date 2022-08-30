3de2786d43ecf8b9e8839824f840aef0b2263786
HIBER97 <hibar9710@gmail.com>
2022-08-30 14:59:47 +0900

    2022. 8. 30. PM 2:59 --------------------------------------- "Q.11652 카드" 시도중
    arr에서 vector로 전환

--- 정렬/Q.11652 카드.cpp
+++ 정렬/Q.11652 카드.cpp
@@ -6,9 +6,12 @@ void Radix_Sort(){
     int N = 10;
     int Test_Data[10] = {012,421,046,674,103,502,007,100,021,545};
     int arr[10];
+    vector<int> vector[10];
 
     for (int i = 0; i < N; i++)
-        if (Test_Data[i] % 10)
+       if (i = Test_Data[i] % 10)
+           arr[i] = Test_Data[i];
+
 
 }
 
@@ -16,4 +19,5 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    Radix_Sort();
 }
