fe411cc88adbde03335b5bb53fe4518e5c2dad73
HIBER97 <hibar9710@gmail.com>
2022-09-10 14:46:45 +0900

    2022. 9. 10. PM 2:46 --------------------------------------- "Q.9095 1,2,3 더하기" 시도

---
+++ 다이나믹 프로그래밍/Q.9095 1,2,3 더하기.cpp
@@ -0,0 +1,26 @@
+//https://www.acmicpc.net/problem/9095
+#include <bits/stdc++.h>
+using namespace std;
+int N,T;
+int arrN[11];
+int arrR[11];
+
+int Result(){
+
+
+}
+
+int OTT(){
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+        cin >> arrN[i];
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+}
