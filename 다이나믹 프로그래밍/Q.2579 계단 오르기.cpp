716bac64995ea38cbfca4bfeda9a78af5b31eb79
HIBER97 <hibar9710@gmail.com>
2022-09-19 10:10:14 +0900

    2022. 9. 19. AM 10:09 --------------------------------------- "Q.2579 계단 오르기" 시도중 as 실패..?

--- 다이나믹 프로그래밍/Q.2579 계단 오르기.cpp
+++ 다이나믹 프로그래밍/Q.2579 계단 오르기.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int arr[301];
 int arrS[10000];
-int N, Total;
+int N = 0, Total = 0;
 
 int main() {
     ios::sync_with_stdio(0);
@@ -12,7 +12,7 @@ int main() {
 
     cin >> N;
 
-    for (int i = 0; i < N; i++){
+    for (int i = 1; i <= N; i++){
         cin >> arrS[i];
         Total += arrS[i];
     }
