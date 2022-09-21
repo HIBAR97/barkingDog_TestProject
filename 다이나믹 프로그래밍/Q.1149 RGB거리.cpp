71265e27da7596797f4052fd0bf1d5fbd7921463
HIBER97 <hibar9710@gmail.com>
2022-09-21 12:26:28 +0900

    2022. 9. 21. PM 12:25 --------------------------------------- "Q.1149 RGB거리" 시도

--- 다이나믹 프로그래밍/Q.1149 RGB거리.cpp
+++ 다이나믹 프로그래밍/Q.1149 RGB거리.cpp
@@ -2,8 +2,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int N;
+int Cost_R[1001], Cost_G[1001], Cost_B[1001];
+int House[1001][3];
+int Total = 0;
+
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    cin >> N;
 
+    for (int i = 0; i < N; i++){
+        cin >> Cost_R[i];
+        cin >> Cost_G[i];
+        cin >> Cost_B[i];
+    }
+
+    House[1][0] = Cost_R[1];
+    House[1][1] = Cost_G[1];
+    House[1][2] = Cost_B[1];
+
+    for (int i = 2; i <= N; i++){
+        House[i][0] = min(House[i -1][1], House[i -1][2] + Cost_R[i]);
+        House[i][1] = min(House[i -1][2], House[i -1][3] + Cost_G[i]);
+        House[i][2] = min(House[i -1][3], House[i -1][4] + Cost_B[i]);
+    }
+
+    cout << min({House[N][0], House[N][1], House[N][2]});
 }
