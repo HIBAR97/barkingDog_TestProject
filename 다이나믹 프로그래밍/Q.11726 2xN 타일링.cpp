cdb49bfbf6de7d2a6a3d7e6fc586b61ea6aaad53
HIBER97 <hibar9710@gmail.com>
2022-09-26 10:01:41 +0900

    2022. 9. 26. AM 10:01 --------------------------------------- "Q.11726" 알고리즘 시도

---
+++ 다이나믹 프로그래밍/Q.11726 2xN 타일링.cpp
@@ -0,0 +1,19 @@
+//https://www.acmicpc.net/problem/11726
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+bool Block[10005];
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> N;
+    
+    Block[1] = 1;
+    Block[2] = 2;
+
+    for (int i = 0; i < N; ++i)
+
+}
