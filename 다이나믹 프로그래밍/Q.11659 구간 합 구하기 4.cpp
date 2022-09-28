baf25b296f9341433ae78bd4da8e953e9f0353d5
HIBER97 <hibar9710@gmail.com>
2022-09-29 08:54:30 +0900

    2022. 9. 29. AM 8:54 --------------------------------------- "Q.11659 구간 합 구하기 4" 알고리즘 구상중

---
+++ 다이나믹 프로그래밍/Q.11659 구간 합 구하기 4.cpp
@@ -0,0 +1,24 @@
+//https://www.acmicpc.net/problem/11659
+#include <bits/stdc++.h>
+using namespace std;
+
+int N,M;
+int arr[10005];
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> M;
+
+    for (int i = 2*M +1; i <= N; i++)
+        cin >> arr[i];
+
+    for (int i = 1; i <= 2*M; i++)
+        cin >> arr[i];
+
+    for (int i = 0; i < 3; i++)
+        for (int j = arr[]; j < ; ++j)
+
+
+}
