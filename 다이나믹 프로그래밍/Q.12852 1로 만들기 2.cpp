47eb1cd8eb309c2a10ed729f16c536ef53b3cba3
HIBER97 <hibar9710@gmail.com>
2022-10-04 09:29:54 +0900

    2022. 10. 4. AM 9:29 --------------------------------------- "Q.12852 1로 만들기 2" 알고리즘 구상

--- 다이나믹 프로그래밍/Q.12852 1로 만들기 2.cpp
+++ 다이나믹 프로그래밍/Q.12852 1로 만들기 2.cpp
@@ -2,8 +2,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int N;
+int arr[100005], Map[100005];
+
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    cin >> N;
+
+    Map[0] = 0;
+    for (int i = 2; i <= N; i++) {
+        Map[i] = Map[i -1] + 1;
+        arr[i] = i -1;
+
+    }
 }
