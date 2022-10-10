288cac98bc3590ac39796d58905affcdee28e0cd
HIBER97 <hibar9710@gmail.com>
2022-10-10 11:42:21 +0900

    2022. 10. 10. AM 11:42 --------------------------------------- "Q.11047 동전 0" 대략 완성

--- 그리디/Q.11047 동전 0.cpp
+++ 그리디/Q.11047 동전 0.cpp
@@ -2,8 +2,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int N,K;
+int Coin_arr[15];
+int Coin = 0;
+
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    cin >> N >> K;
+
+    for (int i = 0; i < N; i++)
+        cin >> Coin_arr[i];
+
+    for (int i = N; i < 0; i--) {
+        if (K % Coin_arr[i] == 0){
+            K /= Coin_arr[i];
+            Coin++;
+        }
+    }
+
+    cout << Coin;
 }
