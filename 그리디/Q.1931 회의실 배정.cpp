697905a984d7fcd128e4a298ef545db61747917d
HIBER97 <hibar9710@gmail.com>
2022-10-12 11:55:50 +0900

    2022. 10. 12. AM 11:54 --------------------------------------- "Q.11047 동전 0" 성공 "Q.1931 회의실 배정" 알고리즘 작성중

---
+++ 그리디/Q.1931 회의실 배정.cpp
@@ -0,0 +1,29 @@
+//https:www.acmicpc.net/problem/1931
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+int T = 0;
+int Result;
+pair <int, int> Time[10000005];
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    for (int i = 0; i < N; i++) 
+        cin >> Time[i].first >> Time[i].second;
+
+    sort(Time, Time +N);
+
+    for (int i = 0; i < N; i++) {
+        if (T > Time[i].first)
+            continue;
+
+        else if (T <= Time[i].first){
+            Result++;
+            T = Time[i].second;
+        }
+    }
+    cout << Result;
+}
