89d7517e550e1b422446574544173f2265a50742
HIBER97 <hibar9710@gmail.com>
2022-10-17 08:40:04 +0900

    2022. 10. 17. AM 8:39 --------------------------------------- "Q.1026 보물" 내일 완성할랭ㅎ

---
+++ 그리디/Q.1026 보물.cpp
@@ -0,0 +1,28 @@
+//https://www.acmicpc.net/problem/1026
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+int A[100], B[100];
+int Result;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+        cin >> A[i];
+
+    for (int i = 0; i < N; i++)
+        cin >> B[i];
+
+    sort(A, A +N);
+    sort(B, B +N);
+
+    for (int i = 0; i < N; i++)
+        Result += A[i] * B[i];
+
+    cout << Result;
+}
