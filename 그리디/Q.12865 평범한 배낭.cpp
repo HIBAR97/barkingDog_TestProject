52c0ede0a72383d0117066e8de073b20042fa0a5
HIBER97 <hibar9710@gmail.com>
2022-10-23 11:18:21 +0900

    2022. 10. 23. AM 11:18 --------------------------------------- "Q.12865 평법한 가방" 틀림

--- 그리디/Q.12865 평범한 배낭.cpp
+++ 그리디/Q.12865 평범한 배낭.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int N,K;
 int Mono[105][100005];
-int W[100005];
+int W[105];
 int V[1005];
 
 
@@ -20,7 +20,7 @@ int main(void) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < K; j++) {
             if (j - W[i] >= 0)
-                Mono[i][j] = max(Mono[i -1][j], Mono[i -1][j -W[i]] + V[i]);
+                Mono[i][j] = max(Mono[i -1][j], Mono[i -1][j] - W[i]] + V[i]);
             else
                 Mono[i][j] = Mono[i -1][j];
         }
