4e9184cefff84dbc7681e68965198a7f12c3515c
HIBER97 <hibar9710@gmail.com>
2022-10-25 08:01:19 +0900

    2022. 10. 25. AM 7:59 --------------------------------------- "Q.1477 휴게소 세우기" 기초 공사

---
+++ 그리디/Q.1477 휴게소 세우기.cpp
@@ -0,0 +1,18 @@
+//https://www.acmicpc.net/problem/1477
+#include <bits/stdc++.h>
+using namespace std;
+
+int N,M,L;
+int Wait[55];
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> M >> L;
+
+    for (int i = 0; i < N; i)
+        cin >> Wait[i];
+
+
+}
