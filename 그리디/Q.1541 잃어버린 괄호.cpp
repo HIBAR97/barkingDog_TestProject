a04616494494be7aad55d763b844350b22189b73
HIBER97 <hibar9710@gmail.com>
2022-10-27 20:39:04 +0900

    2022. 10. 27. PM 8:38 --------------------------------------- "Q.1541" 잃어버린 괄호 시도중

--- 그리디/Q.1541 잃어버린 괄호.cpp
+++ 그리디/Q.1541 잃어버린 괄호.cpp
@@ -4,11 +4,17 @@ using namespace std;
 
 int Num_arr[55];
 string Mass_arr[55];
+string str;
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    cin >> str;
 
+    for (int i = 0; i < str.size(); i++) {
+        if (str[i] == '+' || str[i] == '-' || str[i] == '\n')
+            if ()
+    }
 
 }
