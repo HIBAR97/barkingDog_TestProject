4fd2fd9fae5343f2f9a06c59ed39b270b0c47385
HIBER97 <hibar9710@gmail.com>
2022-11-01 16:19:32 +0900

    2022. 11. 1. PM 4:19 --------------------------------------- "Q.1541" 잃어버린 괄호 시도중

--- 그리디/Q.1541 잃어버린 괄호.cpp
+++ 그리디/Q.1541 잃어버린 괄호.cpp
@@ -2,34 +2,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int Num_arr[55];
-string Mass_arr[55];
-string temp;
+string temp = "";
 string str;
 int Result;
-bool minus = false;
+bool minus = 0;
 
-int main(void) {
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> str;
 
     for (int i = 0; i < str.size(); i++) {
-        if (str[i] == '+' || str[i] == '-' || str[i] == '\n')
-            if (minus){
+        if (str[i] == '+' || str[i] == '-' || str[i] == '\0'){
+            if (minus)
                 Result -= stoi(temp);
-            }
             else
                 Result += stoi(temp);
+
             temp = "";
-
-        if (str[i] == '-'){
-            minus = true;
+            if (str[i] == '-')
+                minus = 1;
+            else
+                temp += str[i];
         }
-        else
-            temp += str[i];
-
     }
     cout << Result;
 }
