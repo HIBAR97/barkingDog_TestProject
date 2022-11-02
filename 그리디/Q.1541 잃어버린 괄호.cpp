e6190731b298a7d4c81f77803420d01a72c3663c
HIBER97 <hibar9710@gmail.com>
2022-11-02 12:05:36 +0900

    2022. 11. 2. PM 12:05 --------------------------------------- "Q.1541" 잃어버린 괄호 완성 알고리즘 순서 문제였음

--- 그리디/Q.1541 잃어버린 괄호.cpp
+++ 그리디/Q.1541 잃어버린 괄호.cpp
@@ -1,11 +1,12 @@
 //https://www.acmicpc.net/problem/1541
 #include <bits/stdc++.h>
+
 using namespace std;
 
 string temp = "";
 string str;
-int Result;
-bool minus = 0;
+int Result = 0;
+bool Minus = false;
 
 int main() {
     ios::sync_with_stdio(0);
@@ -13,19 +14,20 @@ int main() {
 
     cin >> str;
 
-    for (int i = 0; i < str.size(); i++) {
+    for (int i = 0; i <= str.size(); i++) {
         if (str[i] == '+' || str[i] == '-' || str[i] == '\0'){
-            if (minus)
+            if (Minus)
                 Result -= stoi(temp);
             else
                 Result += stoi(temp);
 
             temp = "";
             if (str[i] == '-')
-                minus = 1;
-            else
-                temp += str[i];
+                Minus = true;
         }
+        else
+            temp += str[i];
+
     }
     cout << Result;
 }
