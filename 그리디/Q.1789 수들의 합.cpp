172879edf8868fd5140acfb7a953b268676c64da
HIBER97 <hibar9710@gmail.com>
2022-11-05 09:59:25 +0900

    2022. 11. 5. AM 9:59 --------------------------------------- "Q.1789 수들의 합" 시간초과

--- 그리디/Q.1789 수들의 합.cpp
+++ 그리디/Q.1789 수들의 합.cpp
@@ -2,8 +2,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int N,S;
+int cnt = 1;
+long long Result = 0;
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    cin >> S;
+
+    while (true){
+        if (Result < S)
+            Result += cnt;
+        else if (Result > S) {
+            cnt--;
+            break;
+        }
+    }
+
+    cout << Result;
 }
