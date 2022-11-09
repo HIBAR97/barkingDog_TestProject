f7529f473d2f448735acfeed899f3f281561add6
HIBER97 <hibar9710@gmail.com>
2022-11-09 11:49:30 +0900

    2022. 11. 9. AM 11:48 --------------------------------------- "Q.1463 1로 만들기" 복습중

--- 다이나믹 프로그래밍/Q.1463 1로 만들기.cpp
+++ 다이나믹 프로그래밍/Q.1463 1로 만들기.cpp
@@ -38,6 +38,23 @@ void Make_2(){
     cout << arr[N];
 }
 
+void Fuc3(){
+    arr[0] = 0;
+    for (int i = 1; i < N; i++) {
+        if (N % 3 == 0 || N % 2 == 0)
+            arr[i] = min(N/3,N/2);
+
+        if (N % 3 == 0)
+            arr[i] = N /3;
+
+        if (N % 2 == 0)
+            arr[i] = N /2;
+
+        else
+            arr[i] = N -1;
+    }
+
+}
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
