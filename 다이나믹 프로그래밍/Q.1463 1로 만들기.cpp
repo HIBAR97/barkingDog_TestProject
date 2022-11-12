3754915fc0b3f008b8191d6e7cddf19145644007
HIBER97 <hibar9710@gmail.com>
2022-11-12 19:43:46 +0900

    2022. 11. 12. PM 7:43 --------------------------------------- "Q.1463 1로 만들기" 복습중

--- 다이나믹 프로그래밍/Q.1463 1로 만들기.cpp
+++ 다이나믹 프로그래밍/Q.1463 1로 만들기.cpp
@@ -44,7 +44,7 @@ void Fuc3(){
         arr[i] = arr[i -1] +1;
 
         if (i % 2 == 0)
-            ;
+            arr[i] = min(arr[i], arr[i/2] + 2);
         if (i % 3 == 0)
             ;
     }
