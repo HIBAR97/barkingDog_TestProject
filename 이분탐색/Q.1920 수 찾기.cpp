328846aa44cc8d359eacc4922065a9ba34dad0a4
HIBER97 <hibar9710@gmail.com>
2022-11-23 11:46:07 +0900

    2022. 11. 23. AM 11:45 --------------------------------------- "Q.1920 수 찾기" 완성

--- 이분탐색/Q.1920 수 찾기.cpp
+++ 이분탐색/Q.1920 수 찾기.cpp
@@ -10,7 +10,7 @@ int search(int T){
     int st = 0;
     int end = N-1;
 
-    while (st < end){
+    while (st <= end){
         int mid = (st+end) /2;
         if (arr_N[mid] < T)
             st = mid + 1;
@@ -19,6 +19,8 @@ int search(int T){
         else
             return 1;
     }
+    if (st > end)
+        return 0;
 }
 
 int main() {
@@ -31,16 +33,24 @@ int main() {
     for (int i = 0; i < N; i++)
         cin >> arr_N[i];
 
-    cin >> M;
-    for (int i = 0; i < M; i++)
-        cin >> arr_M[i];
+    sort(arr_N, arr_N+N);
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (arr_N[i] == arr_M[i])
-                cnt = 1;
-        }
-        if (cnt == 1)
-            cout << "1" << "\n";
+//    cin >> M;
+//    for (int i = 0; i < M; i++)
+//        cin >> arr_M[i];
+//
+//    for (int i = 0; i < N; i++) {
+//        for (int j = 0; j < M; j++) {
+//            if (arr_N[i] == arr_M[i])
+//                cnt = 1;
+//        }
+//        if (cnt == 1)
+//            cout << "1" << "\n";
+//    }
+
+    cin >> M;
+    while (M--){
+        cin >> cnt;
+        cout <<  search(cnt) << "\n";
     }
 }
