0bec3646fa009aab0f94da237bfa20a744d5cbfd
HIBER97 <hibar9710@gmail.com>
2022-11-26 10:51:46 +0900

    2022. 11. 26. AM 10:51 --------------------------------------- "Q.10816 숫자 카드 2" 시도중

--- 이분탐색/Q.10816 숫자 카드 2.cpp
+++ 이분탐색/Q.10816 숫자 카드 2.cpp
@@ -6,6 +6,21 @@ int N, M, T;
 int arr_N[500005];
 int arr_M[20000005];
 
+int Result(int T, int l){
+    int start = 0;
+    int end = l;
+
+    while(start < end){
+        int mid = (start + end) /2;
+
+        if (arr_N[mid] >= T)
+            end = mid;
+        else
+            start = mid +1;
+    }
+    return start;
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -20,7 +35,8 @@ int main() {
 
     while (M--){
        cin >> T;
-       cout << ;
+
+       cout << Result(T,N);
     }
 
 }
