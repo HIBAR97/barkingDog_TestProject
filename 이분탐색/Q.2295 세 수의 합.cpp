3e537e7df0e561938507303d7a24fd8e6e027efa
HIBER97 <hibar9710@gmail.com>
2022-12-09 08:59:45 +0900

    2022. 12. 9. AM 8:59 --------------------------------------- "Q.2295 세 수의 합" 시도

--- 이분탐색/Q.2295 세 수의 합.cpp
+++ 이분탐색/Q.2295 세 수의 합.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int N;
 int arr[1005];
+vector<int> Plus;
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -16,8 +18,9 @@ int main() {
 
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
+            Plus.push_back(arr[i] + arr[j]);
 
-
+    sort(Plus.begin(), Plus.end());
 
 
 }
