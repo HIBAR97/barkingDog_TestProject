8f7522dccd92bd8276ddd791ef311acd1d10ec72
HIBER97 <hibar9710@gmail.com>
2022-12-12 08:37:42 +0900

    2022. 12. 12. AM 8:37 --------------------------------------- "Q.1654 랜선 자르기" 시도

---
+++ 이분탐색/Q.1654 랜선 자르기.cpp
@@ -0,0 +1,16 @@
+//https://www.acmicpc.net/problem/1654
+#include <bits/stdc++.h>
+using namespace std;
+
+int K, N;
+int arr[1000005];
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> K >> N;
+
+    for (int i = 0; i < K; i++)
+        cin >> arr[i];
+
+}
