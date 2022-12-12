1aa59e013814b5fdc12e1b2f94330cad3c87d819
HIBER97 <hibar9710@gmail.com>
2022-12-13 08:16:01 +0900

    2022. 12. 13. AM 8:15 --------------------------------------- "Q.1654 랜선 자르기" 시도

--- 이분탐색/Q.1654 랜선 자르기.cpp
+++ 이분탐색/Q.1654 랜선 자르기.cpp
@@ -4,13 +4,33 @@ using namespace std;
 
 int K, N;
 int arr[1000005];
+int Total, Div;
+
+int TF(int Div, int N){
+    int Result;
+
+    for (int i = 0; i < N; i++)
+         Result += arr[i] / Div;
+
+    if (Result == N)
+        return 1;
+    else
+        return 0;
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> K >> N;
 
-    for (int i = 0; i < K; i++)
+    for (int i = 0; i < K; i++){
         cin >> arr[i];
+        Total += arr[i];
+    }
+
+    Div = Total / N;
+
+    if (TF(Div, N))
 
 }
