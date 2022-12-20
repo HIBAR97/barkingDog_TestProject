e411bd8430a4d34d9724510c5f630eb762bd4acc
HIBER97 <hibar9710@gmail.com>
2022-12-20 10:39:40 +0900

    2022. 12. 20. AM 10:39 --------------------------------------- "Q.2230 수 고르기" 시도

--- 투 포인트/Q. 2230 수 고르기.cpp
+++ 투 포인트/Q. 2230 수 고르기.cpp
@@ -2,8 +2,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int N,M;
+int arr[100000005];
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+
 
 }
