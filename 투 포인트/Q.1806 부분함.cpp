42d3b5ae67218505c64ad140863ccf85dded71d9
HIBER97 <hibar9710@gmail.com>
2022-12-28 22:33:10 +0900

    2022. 12. 28. PM 10:33 --------------------------------------- "Q.1806 부분합" 시도

--- 투 포인트/Q.1806 부분함.cpp
+++ 투 포인트/Q.1806 부분함.cpp
@@ -2,8 +2,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int N, S;
+int arr[10005];
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
+
+    cin >> N >> S;
+
+    for (int i = 0; i < N; i++)
+        cin >> arr[i] ;
 
 }
