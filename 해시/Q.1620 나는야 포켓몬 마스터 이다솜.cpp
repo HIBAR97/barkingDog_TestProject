4d0cc4e967b1df89f73c07c095141f9b4626f6ae
HIBER97 <hibar9710@gmail.com>
2023-01-18 08:02:27 +0900

    2023. 1. 18. AM 8:02 --------------------------------------- "Q.1620 나는야 포켓몬 마스터 이다솜" 시도

--- 해시/Q.1620 나는야 포켓몬 마스터 이다솜.cpp
+++ 해시/Q.1620 나는야 포켓몬 마스터 이다솜.cpp
@@ -5,6 +5,7 @@ using namespace std;
 unordered_map<string , int> S_to_Int;
 string Int_to_S[1000005];
 int N,M;
+string Q;
 
 int main() {
     ios::sync_with_stdio(0);
@@ -18,6 +19,9 @@ int main() {
     }
 
     while (M--){
+        cin >> Q;
+        if (isdigit(Q[0]))
+            cout << Int_to_S[stoi(Q)] << '\n';
 
     }
 }
