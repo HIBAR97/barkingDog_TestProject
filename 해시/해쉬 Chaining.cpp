d527b26b12972707b126742bedd95cda1f4a24b3
HIBER97 <hibar9710@gmail.com>
2023-01-23 10:02:31 +0900

    2023. 1. 23. AM 10:02 --------------------------------------- "해쉬 Chaining" 시도중

--- 해시/해쉬 Chaining.cpp
+++ 해시/해쉬 Chaining.cpp
@@ -2,6 +2,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int M = 1000005;
+int A = 1000;
+int MAX = 500005;
+
+int Hash(string& s){
+
+}
+
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
